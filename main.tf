d784fcbdf2806da93664003e336e454f8f0a64ff
Yuki Tsumita <yuki.tsumita@gmail.com>
2023-11-18 18:56:54 +0900

    fix: change public subnet cidr.

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_nat_gateway" "main_nat_gw_1c" {
 ###################################################################
 resource "aws_subnet" "main_public_subnet_1a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "ap-northeast-1a"
   tags = {
     Name = "public_subnet-1a"
@@ -71,7 +71,7 @@ resource "aws_route_table_association" "main_public_subnet_1a" {
 ###################################################################
 resource "aws_subnet" "main_public_subnet_1c" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-1c"
   tags = {
     Name = "public_subnet-1c"
